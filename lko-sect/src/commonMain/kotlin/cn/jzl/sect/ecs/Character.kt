29fd50df7db797953b66f4e34b302a7263d2b6b6
CC <1046697411@qq.com>
2025-12-23 21:21:05 +0800

    添加character依赖的addon

--- lko-sect/src/commonMain/kotlin/cn/jzl/sect/ecs/Character.kt
+++ lko-sect/src/commonMain/kotlin/cn/jzl/sect/ecs/Character.kt
@@ -7,6 +7,7 @@ import cn.jzl.ecs.*
 import cn.jzl.ecs.addon.createAddon
 import cn.jzl.ecs.query.ECSDsl
 import cn.jzl.sect.ecs.cultivation.CultivationService
+import cn.jzl.sect.ecs.cultivation.cultivationAddon
 
 
 sealed class Character
@@ -14,6 +15,7 @@ sealed class Character
 val characterAddon = createAddon("character") {
     install(healthAddon)
     install(levelingAddon)
+    install(cultivationAddon)
     injects { this bind singleton { new(::CharacterService) } }
     components {
         world.componentId<Character> { it.tag() }
